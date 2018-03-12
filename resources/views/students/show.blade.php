bfc1658e2d7df7a16965fec18e6901afd2d28ced
saimaharis <saima.harisahmed@gmail.com>
2018-03-12 12:27:05 +0500

    view updated

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -1,31 +1,105 @@
-@extends('students.layout')
-
+@extends('layouts.admin')
 
 @section('content')
     <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2> Show Student Record</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('students.index') }}"> Back</a>
-            </div>
+        <div class="col-lg-12">
+            <h1 class="page-header"> Student Record</h1>
         </div>
+        <!-- /.col-lg-12 -->
     </div>
+    <!-- /.row -->
+    <form method="post" action="{{route('students.update',$student->id) }}" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
 
 
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                {{ $student->name }}
+
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label >Student Name</label>
+                                    {{ $student->name }}
+                                </div>
+
+
+
+                                <div class="form-group">
+                                    <label>Father's Name </label>
+                                   {{ $student->father_name}}
+
+                                </div>
+                                <div class="form-group">
+                                    <label>Gender</label>
+                                    {{ $student->gender }}
+
+
+                                </div>
+                                <div class="form-group">
+                                    <label>Religion</label>
+                                   {{ $student->religion }}
+                                </div>
+                                <div class="form-group">
+                                    <label>Address</label>
+                                   {{ $student->address }}
+                                </div>
+                                <div class="form-group">
+                                    <label>Phone</label>
+                                   {{ $student->phone }}
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    {{ $student->email }}
+
+                                </div>
+                                <div class="form-group">
+                                    <label>Date of Birth</label>
+                                  {{ $student->date_of_birth }}
+
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Apply For Class</label>
+                                  {{ $student->for_class }}
+                                </div>
+
+
+
+
+
+                            </div>
+                            <!-- /.col-lg-6 (nested) -->
+                        </div>
+                        <!-- /.row (nested) -->
+
+                    </div>
+                    <!-- /.panel-body -->
+                 
+                </div>
+                <!-- /.panel -->
             </div>
+            <!-- /.col-lg-12 -->
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Father Name:</strong>
-                {{ $student->father_name }}
-            </div>
-        </div>
-    </div>
+        <!-- /.row -->
+    </form>
+
 @endsection
+
+
